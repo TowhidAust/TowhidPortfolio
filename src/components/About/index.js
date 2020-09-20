367cb5caf81bd1b3b1550a3965e11888390ca52e
TowhidAust <007towhid2016@gmail.com>
2020-09-20 22:04:43 +0600

    about page services design started

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,3 +1,4 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { Component } from 'react'
 import { Button } from 'react-bootstrap';
 import { CodeBlock, dracula } from "react-code-blocks";
@@ -16,8 +17,6 @@ export default class About extends Component {
         isDataLoaded: true
       })
       console.log("isdata loaded", this.state.isDataLoaded);
-
-
     }
     
     MyCoolCodeBlock() {
@@ -70,7 +69,7 @@ export default class Myself extends Component {
                   </div>
 
                   <div className="aboutMe">
-                    <h1 className="aboutMeName">Hi, I am Md. Towhidul Islam</h1>
+                    <h1 className="aboutMeName">I am <span> Md. Towhidul Islam</span></h1>
                     <p>I am a frontend web developer. I can provide clean code and pixel perfect design. I also make website more & more interactive with web animations.</p>
                     <p>FullName &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: Md. Towhidul Islam</p>
                     <p>Age&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: 26</p>
@@ -81,6 +80,37 @@ export default class Myself extends Component {
                   </div>
                 </div>
                 
+                <section className="serviceSection">
+
+                  <div className="servicesHeader">
+                    <h1>Services</h1>
+                    <hr/>
+                  </div>
+
+                  <div className="serviceContents">
+                    <div className="s_WebDesign">
+                    <FontAwesomeIcon icon={["fal", "coffee"]} />
+                      <h3>Web Design</h3>
+                      <hr />
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem tenetur ratione quod.</p>
+                    </div>
+
+                    <div className="s_WebDevelopment">
+                      <span>Icon</span>
+                      <h3>Web Development</h3>
+                      <hr />
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem tenetur ratione quod.</p>
+                    </div>
+
+                    <div className="s_MobileAppDevelopment">
+                      <span>Icon</span>
+                      <h3>Mobile App</h3>
+                      <hr />
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem tenetur ratione quod.</p>
+                    </div>
+                  </div>
+
+                </section>
               </div>
             </div>
           </div>
