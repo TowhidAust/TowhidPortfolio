d4d05030b2528188cac6e1c91bb71de39d867c42
TowhidAust <007towhid2016@gmail.com>
2020-09-29 22:32:14 +0600

    changes

--- towhid_portfolio/src/components/Resume/index.js
+++ towhid_portfolio/src/components/Resume/index.js
@@ -4,8 +4,10 @@ import './resume.css';
 export default class Resume extends Component {
     render() {
         return (
-            <div>
-                <div style={{textAlign:"center"}}>Resume coming soon</div>
+            <div className="resumeContainer">
+                <div className="resumeInner">
+                    askh
+                </div>
             </div>
         )
     }
