4f56e913d2c1f063734ad84e7fb0588acb7e2909
TowhidAust <towhidaustcse33@gmail.com>
2020-10-03 16:16:22 +0600

    resume update

--- towhid_portfolio/src/components/Resume/index.js
+++ towhid_portfolio/src/components/Resume/index.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
 import './resume.css';
-
+import LinearProgress from '@material-ui/core/LinearProgress';
 export default class Resume extends Component {
     render() {
         return (
             <div className="resumeContainer">
                 <div className="resumeInner">
-                <Button variant="contained" color="primary">
-                    Hello World
-                </Button>
+                    <section className="mySkillsSection">
+                        <div className="skillHeadingCont">
+                            <h1 className="mySkillsHeading">My Skills</h1>
+                            <hr/>
+                        </div>
+                        <div className="skillDetails">
+                            <span>HTML</span>
+                            <LinearProgress variant="buffer" value='95' valueBuffer= '95' />
+                        </div>
+                    </section>
                 </div>
             </div>
         )
