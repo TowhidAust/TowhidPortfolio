ac88c16e6e365354eea36da5c0d9758243edaaa1
TowhidAust <towhidaustcse33@gmail.com>
2020-10-03 21:37:44 +0600

    resume css designs

--- towhid_portfolio/src/components/Resume/index.js
+++ towhid_portfolio/src/components/Resume/index.js
@@ -12,8 +12,84 @@ export default class Resume extends Component {
                             <hr/>
                         </div>
                         <div className="skillDetails">
-                            <span>HTML</span>
-                            <LinearProgress variant="buffer" value='95' valueBuffer= '95' />
+                            <div className="skills">
+                                <span>HTML5</span>
+                                <LinearProgress variant="buffer" value='90' valueBuffer= '95' />
+                            </div>
+
+                            <div className="skills">
+                                <span>CSS3</span>
+                                <LinearProgress variant="buffer" value='90' valueBuffer= '95' />
+                            </div>
+
+                         
+                            <div className="skills">
+                                <span>BOOTSTRAP4</span>
+                                <LinearProgress variant="buffer" value='90' valueBuffer= '95' />
+                            </div>
+
+                            <div className="skills">
+                                <span>JQUEY</span>
+                                <LinearProgress variant="buffer" value='90' valueBuffer= '95' />
+                            </div>
+                        
+                            <div className="skills">
+                                <span>D3JS</span>
+                                <LinearProgress variant="buffer" value='90' valueBuffer= '95' />
+                            </div>
+                            <div className="skills">
+                                <span>WEBPACK</span>
+                                <LinearProgress variant="buffer" value='90' valueBuffer= '95' />
+                            </div>
+                            <div className="skills">
+                                <span>JAVASCRIPT</span>
+                                <LinearProgress variant="buffer" value='90' valueBuffer= '95' />
+                            </div>
+                            <div className="skills">
+                                <span>FIREBASE</span>
+                                <LinearProgress variant="buffer" value='90' valueBuffer= '95' />
+                            </div>
+                            <div className="skills">
+                                <span>NODEJS</span>
+                                <LinearProgress variant="buffer" value='90' valueBuffer= '95' />
+                            </div>
+                            <div className="skills">
+                                <span>EXPRESSJS</span>
+                                <LinearProgress variant="buffer" value='90' valueBuffer= '95' />
+                            </div>
+                            <div className="skills">
+                                <span>GIT</span>
+                                <LinearProgress variant="buffer" value='90' valueBuffer= '95' />
+                            </div>
+                            <div className="skills">
+                                <span>JSON</span>
+                                <LinearProgress variant="buffer" value='90' valueBuffer= '95' />
+                            </div>
+                            <div className="skills">
+                                <span>REST API</span>
+                                <LinearProgress variant="buffer" value='90' valueBuffer= '95' />
+                            </div>
+                            <div className="skills">
+                                <span>PAYMENT GATEWAY INTEGRATION, SSL COMMERZ, PAYPAL ETC.</span>
+                                <LinearProgress variant="buffer" value='90' valueBuffer= '95' />
+                            </div>
+
+                            <div className="skills">
+                                <span>REACT JS</span>
+                                <LinearProgress variant="buffer" value='90' valueBuffer= '95' />
+                            </div>
+                            <div className="skills">
+                                <span>REACT NATIVE</span>
+                                <LinearProgress variant="buffer" value='90' valueBuffer= '95' />
+                            </div>
+                            <div className="skills">
+                                <span>MATERIAL UI</span>
+                                <LinearProgress variant="buffer" value='90' valueBuffer= '95' />
+                            </div>
+                            <div className="skills">
+                                <span>SERVER SIDE PROGRAMMING</span>
+                                <LinearProgress variant="buffer" value='90' valueBuffer= '95' />
+                            </div>
                         </div>
                     </section>
                 </div>
