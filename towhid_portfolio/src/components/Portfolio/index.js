8a02f1c6dd8e493422bf5d55ac22d8f5de0f6015
TowhidAust <007towhid2016@gmail.com>
2020-10-12 23:39:43 +0600

    added portfolio designs

--- towhid_portfolio/src/components/Portfolio/index.js
+++ towhid_portfolio/src/components/Portfolio/index.js
@@ -4,9 +4,48 @@ import './portfolio.css';
 export default class Portfolio extends Component {
     render() {
         return (
-            <div>
-                <div style={{textAlign:"center"}}>Portfolio coming soon</div>
-            </div>
+            <>
+                <div className="portfolioContainer">
+                    <div className="protfolioHeading">
+                        <h1>My Portfolio</h1>
+                        <hr/>
+                    </div>
+
+                    <div className="portfolioDetails">
+                        <div className="card_">
+
+                        <div className="portfolioImage">
+                            <img src="https://ourcodeworld.com/public-media/articles/articleocw-5d07e6b3790af.jpg" alt="portfolio_image"/>        
+                        </div>
+                        <h3>Web Design</h3>
+                        <hr />
+                        <p>The most important part is frontend. I can code with beautiful design and animations. I also ensure responsiveness and browser compatibility. User friendly neat and clean design pattern with media queries keyframes and modern css animations.</p>
+                        </div>
+
+                        <div className="card_">
+
+                        <div className="portfolioImage">
+                            <img src="https://ourcodeworld.com/public-media/articles/articleocw-5d07e6b3790af.jpg" alt="portfolio_image"/>        
+                        </div>
+                        <h3>Web Design</h3>
+                        <hr />
+                        <p>The most important part is frontend. I can code with beautiful design and animations. I also ensure responsiveness and browser compatibility. User friendly neat and clean design pattern with media queries keyframes and modern css animations.</p>
+                        </div>
+
+                        <div className="card_">
+
+                        <div className="portfolioImage">
+                            <img src="https://ourcodeworld.com/public-media/articles/articleocw-5d07e6b3790af.jpg" alt="portfolio_image"/>        
+                        </div>
+                        <h3>Web Design</h3>
+                        <hr />
+                        <p>The most important part is frontend. I can code with beautiful design and animations. I also ensure responsiveness and browser compatibility. User friendly neat and clean design pattern with media queries keyframes and modern css animations.</p>
+                        </div>
+                    </div>
+
+                    
+                </div>
+            </>
         )
     }
 }
